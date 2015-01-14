aec9e88e6d52a859124849bebfd49f962c9c9546
Valeriy Goncharenko <virus-x@ukr.net>
2015-01-14 15:48:32 +0200

    Refactored msgpack mapper, added depth limit.

--- src/java/com/stratio/cassandra/index/schema/ColumnMapperMgsPack.java
+++ src/java/com/stratio/cassandra/index/schema/ColumnMapperMgsPack.java
@@ -1,6 +1,7 @@
 package com.stratio.cassandra.index.schema;
 
 import com.stratio.cassandra.index.util.ByteBufferUtils;
+import org.apache.cassandra.cql3.Constants;
 import org.apache.cassandra.db.marshal.AbstractType;
 import org.apache.cassandra.db.marshal.AsciiType;
 import org.apache.cassandra.db.marshal.BytesType;
@@ -16,9 +17,13 @@ import org.apache.lucene.document.StringField;
 import org.apache.lucene.search.SortField;
 import org.apache.lucene.search.SortField.Type;
 import org.codehaus.jackson.annotate.JsonCreator;
+import org.codehaus.jackson.annotate.JsonProperty;
 import org.msgpack.core.MessageFormat;
 import org.msgpack.core.MessagePack;
 import org.msgpack.core.MessageUnpacker;
+import org.msgpack.value.ArrayValue;
+import org.msgpack.value.MapCursor;
+import org.msgpack.value.ValueRef;
 import org.msgpack.value.holder.ValueHolder;
 
 import java.io.IOException;
@@ -26,17 +31,26 @@ import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by vgoncharenko on 13.01.2015.
  */
 public class ColumnMapperMgsPack extends ColumnMapper<String> {
+
+    /** The default limit for depth of message pack fields. */
+    private static final int DEFAULT_DEPTH_LIMIT = 2;
+
+    /** The maximum depth of object field */
+    private final int fieldDepthLimit;
+
     /**
      * Builds a new {@link ColumnMapperBlob}.
      */
     @JsonCreator
-    public ColumnMapperMgsPack() {
+    public ColumnMapperMgsPack(@JsonProperty("depthLimit") Integer depthLimit) {
         super(new AbstractType<?>[]{AsciiType.instance, UTF8Type.instance, BytesType.instance}, new AbstractType[]{});
+        fieldDepthLimit = depthLimit == null ? DEFAULT_DEPTH_LIMIT : depthLimit;
     }
 
     /**
@@ -73,27 +87,8 @@ public class ColumnMapperMgsPack extends ColumnMapper<String> {
         MessageUnpacker unpacker = MessagePack.newDefaultUnpacker(bytes);
         try {
             ValueHolder v = new ValueHolder();
-            MessageFormat format = unpacker.unpackValue(v);
-            switch (format.getValueType()) {
-                case NIL:
-                    return new ArrayList<Field>();
-                case BOOLEAN:
-                    return Arrays.asList((Field)new StringField(name, v.get().asBoolean().toBoolean() ? "true" : "false", STORE));
-                case INTEGER:
-                    return Arrays.asList((Field)new LongField(name, v.getIntegerHolder().asLong(), STORE));
-                case FLOAT:
-                    return Arrays.asList((Field)new DoubleField(name, v.getFloatHolder().toDouble(), STORE));
-                case STRING:
-                    return Arrays.asList((Field)new StringField(name, v.get().asString().toString(), STORE));
-                case BINARY:
-                    return Arrays.asList((Field)new StringField(name, Hex.bytesToHex(v.get().asBinary().toMessageBuffer().getArray()), STORE));
-                case ARRAY:
-                case MAP:
-                case EXTENDED:
-                    // NOT Supported yet
-                    return new ArrayList<Field>();
-            }
-
+            unpacker.unpackValue(v);
+            return extractFields(name, v.getRef(), 0);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -123,6 +118,58 @@ public class ColumnMapperMgsPack extends ColumnMapper<String> {
     @Override
     public String toString() {
         return new ToStringBuilder(this).toString();
+    }
+
+    private List<Field> extractFields(String name, ValueRef v, int depth){
+        ArrayList<Field> fields = new ArrayList<Field>();
+        switch (v.getValueType()) {
+            case NIL:
+            case EXTENDED:
+                // NOT Supported
+                break;
+            case BOOLEAN:
+                fields.add(new StringField(name, v.asBoolean().toBoolean() ? "true" : "false", STORE));
+                break;
+            case INTEGER:
+                fields.add(new LongField(name, v.asNumber().asLong(), STORE));
+                break;
+            case FLOAT:
+                fields.add(new DoubleField(name, v.asFloat().toDouble(), STORE));
+                break;
+            case STRING:
+                fields.add(new StringField(name, v.asString().toString(), STORE));
+                break;
+            case BINARY:
+                fields.add(new StringField(name, Hex.bytesToHex(v.asBinary().toMessageBuffer().getArray()), STORE));
+                break;
+            case ARRAY:
+                for (ValueRef val : v.getArrayCursor())                {
+                    fields.addAll(extractFields(name,val, depth));
+                }
+                break;
+            case MAP:
+                if (depth >= fieldDepthLimit){
+                    // Depth limit reached
+                    break;
+                }
+
+                MapCursor cursor = v.getMapCursor();
+                while(cursor.hasNext()){
+                    ValueRef key = cursor.nextKeyOrValue();
+                    ValueRef val = cursor.nextKeyOrValue();
+
+                    if (key.isNumber() || key.isBoolean() || key.isString()){
+                        String keyName = name + "." + key.toString();
+                        fields.addAll(extractFields(keyName,val, depth + 1));
+                    }else{
+                        // Key cannot have type map or array
+                        continue;
+                    }
+                }
+                break;
+        }
+
+        return fields;
     }
 
     private byte[] asBytes(Object value) {
